c32c62fd1d4cad0ed15dc5964dc3551be48416c1
Scott <tangscott959@gmail.com>
2022-09-18 01:19:23 -0600

    add filter, Category

---
+++ src/main/java/com/example/quiz_project/domain/Category.java
@@ -0,0 +1,14 @@
+package com.example.quiz_project.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Category {
+    private int CategoryId;
+    private String CategoryName;
+
+}
